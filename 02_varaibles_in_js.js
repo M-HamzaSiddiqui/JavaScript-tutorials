8835a34f260b9a7d436b8bffb958c49d1fd2a65c
Mohd Hamza <userhamza01@gmail.com>
2023-07-30 16:23:20 +0530

    tut2 done

---
+++ 02_varaibles_in_js.js
@@ -0,0 +1,12 @@
+let a= "hamza"
+console.log(a)
+
+a=7
+console.log(a)
+
+// let 8variable=8   //this is not allowed
+
+// let var=7          //this is also not allowed because there are some keywords reserved in javascript
+
+
+//javascript is case sensitive so hamza and Hamza are two different variables.
