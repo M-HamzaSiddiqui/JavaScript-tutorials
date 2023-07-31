f5dcb33979416b36a79e148a3ea8f36a2ea896a2
Mohd Hamza <userhamza01@gmail.com>
2023-07-31 20:11:22 +0530

    Operators in JavaScript

---
+++ 05_Operators_in_JS.js
@@ -0,0 +1,44 @@
+//Operators in JavaScript
+
+a=5;
+b=4;
+
+//Arithematic Operators
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)   //this is a raised to power of b
+console.log("a%b=",a%b)   
+console.log("a++=",a++)   
+console.log("++a=",++a)   
+console.log("--b=",--b)   
+
+//Assignment Operators
+
+let assignment=1;
+console.log(assignment+=5);
+console.log(assignment-=5);
+console.log(assignment*=5);
+console.log(assignment/=5);
+console.log(assignment%=5);
+console.log(assignment**=5);
+
+//Comparison Operators
+
+let comp1=6;
+let comp2="6";
+console.log("comp1==comp2 is ",comp1==comp2);
+console.log("comp1!=comp2 is ",comp1!=comp2);
+console.log("comp1===comp2 is ",comp1===comp2); //this check for type and value both
+console.log("comp1!==comp2 is ",comp1!==comp2);
+console.log("comp1==comp2 is ",comp1==comp2);
+console.log("comp1==comp2 is ",comp1==comp2);
+
+//logical opertors
+
+let n=5;
+let m=6;
+console.log(n<m && n==5);
+console.log(n<m || n==5);
+console.log(!false);
