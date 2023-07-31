8433a12c094b9e6ea851924ce7f203a8fbc05513
Mohd Hamza <userhamza01@gmail.com>
2023-07-31 16:27:32 +0530

    practise set 1

---
+++ practice_set_1.js
@@ -0,0 +1,36 @@
+// Q-1
+
+let a="Hamza"
+let b=5
+console.log(a+b);
+
+//Q-2
+
+console.log(typeof(a+b));
+
+//Q-3
+
+const obj={
+  name:'Hamza',
+  profession:"coding",
+  age:20
+};
+
+// obj=4 //this is not possible
+
+// Q-4
+
+obj['laptop']='dell';
+console.log(obj);
+console.log(obj.profession);
+
+//Q-5
+
+const dict={
+  statement:'a communication or declaration in speech or writing, setting forth facts, particulars, etc.',
+  Aalborg:'a city and port in Denmark, in N Jutland. Pop: 121 549 (2004 est)'
+
+
+}
+
+console.log(dict)
