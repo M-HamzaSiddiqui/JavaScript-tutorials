4e12c29f8236b8a634d930cc60734abea640355a
Mohd Hamza <userhamza01@gmail.com>
2023-07-31 15:59:25 +0530

    primitives and objects in JS

---
+++ 04_primitives_and_objects_in_JS.js
@@ -0,0 +1,26 @@
+// NN BB SS UU  //Primitive data types in javascript.
+
+let a=null;
+let b=342;
+
+let c=true;
+let d=BigInt("573");
+
+let e="hello";
+let f=Symbol("I am a nice symbol");
+
+let g=undefined;
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof(d));
+
+//Non Primitive datatypes in javascript-Objects in javascript
+
+const items={
+  'h':true,
+  'i':false,
+  'j':23,
+  'k':undefined
+}
+
+console.log(items['h']);
