9ff2cc6027aae276b9a4782a0724f4b2a9da5e31
Mohd Hamza <userhamza01@gmail.com>
2023-07-31 11:15:34 +0530

    const,let and var in Javascript

---
+++ 03_const_let_and_var_in_JS.js
@@ -0,0 +1,18 @@
+console.log("JS tutorial 3 var, let and const");
+// var a ='this'   //var can be updated and redeclared within its scope
+// var b=6;
+
+{
+  // var b=5;
+  // console.log(b); //gives 5
+}
+// console.log(b);  //this also gives 5
+
+let a=5
+// let a=4 // let cannot be redeclared
+a=4  //let can be updated but not redaclared
+
+const author="Hamza" // const can niether be redeclared nor be updated.
+
+console.log(author);  //const cannot be left uninitialized
+
