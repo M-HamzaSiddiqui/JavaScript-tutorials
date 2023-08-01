5c515da3896efec608e10a673423d67974113bc9
Mohd Hamza <userhamza01@gmail.com>
2023-08-01 17:00:54 +0530

    funtions in javascript added

---
+++ 09_funcion_in_JS.js
@@ -0,0 +1,21 @@
+//functions in javascript
+
+function onePlusAvg(x,y){
+    console.log("Done");
+    return 1+(x+y)/2;
+  }
+  
+  const sum =(p,q)=>{
+    return p+q;
+  }
+  
+  const hello=()=>{
+    console.log("hello how are you?");
+  }
+  
+  let a=5,b=3,c=6;
+  console.log("the avaerage of a and b is ",onePlusAvg(a,b));
+  console.log("the avaerage of b and c is ",onePlusAvg(b,c));
+  console.log("the avaerage of a and c is ",onePlusAvg(a,c));
+  console.log("the sum of a and b is ",sum(a,b));
+  hello();
