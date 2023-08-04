90ca894319fd9b3795a809a49b3eb92a118a3ccf
Mohd Hamza <userhamza01@gmail.com>
2023-08-04 20:27:32 +0530

    BOM DOM and window object

---
+++ 19_BOM_DOM_and_window_object.js
@@ -0,0 +1,11 @@
+// The window object is the topmost object of DOM hierarchy
+//  It represents a browser window or frame that displays the contents of the webpage.
+
+//It basically provides a way to control the browser window
+
+//DOM :- The Document Object Model (DOM) is a programming interface for web documents. The complete HTML is available as an object named document
+
+
+//BOM:- Browser Object Model. It provides extra objects provided by the host environment (browser) //alert prompt and confirm are part of BOM.
+
+
