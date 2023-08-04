89ebbfa9a0de827fef67e66fbcbb56bf108a2d51
Mohd Hamza <userhamza01@gmail.com>
2023-08-04 19:46:58 +0530

    console object in javascript

---
+++ 17_Javascript_console_object.js
@@ -0,0 +1,20 @@
+console.log(console)  //shows all the available console objects
+//console.error("this is an error")    this displays an error
+//console.assert(5<34)    this throws an error is the assertion fails
+
+//obj={a:1,b:2,c:3}
+//console.table(obj)  it gives a tabular view of an object
+
+// console.warn("display warning")    this displays a warning
+
+// console.info("this is an information")  this is very much similar to log
+
+//console.time("p") and console.timeEnd("p")   this return the time taken to complete a process
+
+/*
+console.time("forLoop")
+for(let i=0;i<500;i++)
+  console.log(i)
+
+console.timeEnd("forLoop")
+*/
